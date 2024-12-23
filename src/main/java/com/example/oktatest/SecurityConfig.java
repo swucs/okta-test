beae7b14bf0eb59ef391ad4770da22df29a44972
박한나/오비고 <swucs7@gmail.com>
2024-12-23 13:54:39 +0900

    oauth2 잠시 테스트 위해 변경

--- src/main/java/com/example/oktatest/SecurityConfig.java
+++ src/main/java/com/example/oktatest/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.oktatest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,18 +13,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .anyRequest().authenticated()
-//                )
-//                .oauth2Login(Customizer.withDefaults());
-//        ;
-
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .oauth2Login(Customizer.withDefaults());
+        ;
+
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/").permitAll()
+//                        .anyRequest().authenticated()
+//                );
 
         return http.build();
 
