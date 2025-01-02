8ec799ed08edcf79432a98e420fe6a570d6e258d
박한나/오비고 <swucs7@gmail.com>
2025-01-02 16:54:36 +0900

    auth0 test

--- src/main/java/com/example/oktatest/HomeController.java
+++ src/main/java/com/example/oktatest/HomeController.java
@@ -1,7 +1,6 @@
 package com.example.oktatest;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,18 +8,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Value("${auth0.toyota.domain}")
+    private String toyotaDomain;
+
+    @Value("${auth0.toyota.client-id}")
+    private String toyotaClientId;
+
+    @Value("${auth0.toyota.redirect-uri}")
+    private String toyotaRedirectUri;
+
+    @Value("${auth0.lexus.domain}")
+    private String lexusDomain;
+
+    @Value("${auth0.lexus.client-id}")
+    private String lexusClientId;
+
+    @Value("${auth0.lexus.redirect-uri}")
+    private String lexusRedirectUri;
+
+
+
     @GetMapping(value = "/")
-    public String home(Model model, @AuthenticationPrincipal final OidcUser oidcUser) {
+    public String home(Model model) {
+        model.addAttribute("toyotaDomain", toyotaDomain);
+        model.addAttribute("toyotaClientId", toyotaClientId);
+        model.addAttribute("toyotaRedirectUri", toyotaRedirectUri);
+        model.addAttribute("lexusDomain", lexusDomain);
+        model.addAttribute("lexusClientId", lexusClientId);
+        model.addAttribute("lexusRedirectUri", lexusRedirectUri);
 
-        if (oidcUser != null) {
-
-            System.out.println("oidcUser.getAccessTokenHash() = " + oidcUser.getAccessTokenHash());
-            System.out.println("oidcUser.getIdToken().getTokenValue() = " + oidcUser.getIdToken().getTokenValue());
-            System.out.println("oidcUser.getEmail() = " + oidcUser.getEmail());
-            System.out.println("oidcUser.getFullName() = " + oidcUser.getFullName());
-
-            model.addAttribute("accessToken", oidcUser.getAccessTokenHash());
-        }
         return "index";
     }
 }
